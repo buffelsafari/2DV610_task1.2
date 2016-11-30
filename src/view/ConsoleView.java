75bec854b55b6c0840d96c4fbd918f2d31a49130
buffel <buffel@buffel-Dator>
2016-11-30 19:28:21 +0100

    GREEN

--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -6,15 +6,19 @@ import java.io.PrintStream;
 
 public class ConsoleView 
 {
+	PrintStream out;
 	// should display stuff on the console and get inputs from keyboard
 	public ConsoleView(PrintStream out, InputStream in)
 	{
-		
+		this.out=out;
 	}
 	
 	// should show a welcomemessage and instructions
 	public void showWelcomeMessage()
 	{
-		
+		out.println("Welcome to Dr. Nim's strategy game.\n"
+				+"Pick any number of sticks from one of the heaps.\n"
+				+"The player who picks the last stick win.\n"
+				+"type a1 to remove 1 stick from heap A...\n");
 	}
 }
