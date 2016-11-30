229d750ff4d71cf126f74b4078259d54d1e2ec2d
buffel <buffel@buffel-Dator>
2016-11-30 19:31:22 +0100

    REFACTOR

--- Test/ConsoleViewTest.java
+++ Test/ConsoleViewTest.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.*;
 import java.io.InputStream;
 import java.io.PrintStream;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -10,18 +11,28 @@ import view.ConsoleView;
 
 public class ConsoleViewTest 
 {
+	PrintStream printStream;
+	InputStream inputStream;		
+	ConsoleView sut;
+	
 	private final static String welcomeString="Welcome to Dr. Nim's strategy game.\n"
 			+"Pick any number of sticks from one of the heaps.\n"
 			+"The player who picks the last stick win.\n"
 			+"type a1 to remove 1 stick from heap A...\n";
+	
+	@Before
+	public void setup()
+	{
+		printStream=Mockito.mock(PrintStream.class);
+		inputStream=Mockito.mock(InputStream.class);		
+		sut=new view.ConsoleView(printStream, inputStream);
+	}
+	
+	
 	@Test
 	public void test() 
 	{
-		PrintStream printStream=Mockito.mock(PrintStream.class);
-		InputStream inputStream=Mockito.mock(InputStream.class);		
-		ConsoleView sut=new view.ConsoleView(printStream, inputStream);
-		sut.showWelcomeMessage();
-		
+		sut.showWelcomeMessage();		
 		Mockito.verify(printStream).println(welcomeString);
 		
 	}
