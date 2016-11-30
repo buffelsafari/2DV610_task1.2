5690792da0ff1322aede5777b6322c8d1bb6dd6e
buffel <buffel@buffel-Dator>
2016-11-30 15:21:23 +0100

    REFACTOR

--- src/model/ComputerPlayer.java
+++ src/model/ComputerPlayer.java
@@ -4,6 +4,8 @@ public class ComputerPlayer
 {
 	// should remove atleast one stick from one of the heaps
 	// Should try to make a winning move if possible
+	// If the player get the heaps to a zero nimSum, the player will win(if no mistakes are made)
+	// nimSum is a^b^c
 	public void MakeMove(Game game)
 	{
 		int a=game.getHeapA();
