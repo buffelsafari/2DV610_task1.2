511071e370e8f96b07dacec1f8bf634e8e2482ff
buffel <buffel@buffel-Dator>
2016-12-01 12:48:09 +0100

    RED

---
+++ Test/ControllerTest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class ControllerTest 
+{
+
+	controller.Controller sut;
+	model.Game game;
+	view.ConsoleView view;
+	model.ComputerPlayer computer;
+	
+	@Before
+	public void setup()
+	{
+		sut=new controller.Controller();
+		game=Mockito.mock(model.Game.class);
+		view=Mockito.mock(view.ConsoleView.class);
+		computer=Mockito.mock(model.ComputerPlayer.class);
+	}
+	
+	@Test
+	public void play_shouldcallShowWelcomeMessage() 
+	{
+		Mockito.when(game.isGameOver()).thenReturn(true);  // else we get stuck in a loop later
+		sut.play(game, view, computer);
+		Mockito.verify(view, Mockito.times(1)).showWelcomeMessage();		
+	}
+}
--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,6 +1,30 @@
 package controller;
 
+import model.ComputerPlayer;
+import model.Game;
+import view.ConsoleView;
+
+
 public class Controller 
 {
 
+	// starts the game
+	public boolean play(Game game, ConsoleView view, ComputerPlayer computer)
+	{
+		// should show welcome message
+		// show heaps
+		
+		// should start a gameloop and loop until game.isGameOver
+		// {
+		
+		// 	1. human player move
+		//	2. test for win(is game over)
+		//  3. computerMove
+		// 	4. test for win(is game over)
+		
+		// }
+		
+		
+		return false;
+	}
 }
