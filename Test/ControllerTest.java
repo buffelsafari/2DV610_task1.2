53f2fae0c7dcab941ce43776365f93ab61dd8ba8
buffel <buffel@buffel-Dator>
2016-12-01 17:31:13 +0100

    RED

--- Test/ControllerTest.java
+++ Test/ControllerTest.java
@@ -95,4 +95,14 @@ public class ControllerTest
 		Mockito.verify(view, Mockito.times(2)).showHeaps(game.getHeapA(), game.getHeapB(), game.getHeapC()); // 2 times
 	}
 	
+	@Test
+	public void play_shouldcallShowYouWin()
+	{
+		Mockito.when(game.isGameOver()).thenReturn(false).thenReturn(true); 			// enter loop and exit the next test
+		Mockito.when(view.input()).thenReturn(true);  									// good input
+		Mockito.when(game.remove(view.getHeap(), view.getNumber())).thenReturn(true);  	// valid remove
+		sut.play(game, view, computer);
+		Mockito.verify(view, Mockito.times(1)).showYouWin();
+	}
+	
 }
